f4df4d9a5d0a1c44a20bf6acbee45d83dbe0d8a7
Nicolas Lochet <nlo@connecthings.com>
2016-05-27 12:03:53 +0200

    add duplicate search in lexicon

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ try {
     '\n'+
     '      Options\n'+
     '        -c, --count\tcount lexicon entries\n'+
+    '        -d, --duplicate\tfind duplicate lexicon entries\n'+
+    '        -S, --stacktrace\tshow stacktrace\n'+
     '\n'+
     '    $ gloss2text <grammarfile> <glossfile>\n'+
     '\n'+
@@ -30,7 +32,8 @@ try {
         o: 'output',
         i: 'interlinear',
         c: 'count',
-        
+        d: 'duplicate',
+        S: 'stacktrace'
       }
     }
   )
@@ -49,7 +52,10 @@ try {
     if (cli.input.length==1) {
       var grammar_analyser = require(__dirname+'/lib/grammar_analyser.js')(parse(fs.readFileSync(cli.input[0], 'utf8')))
       if (cli.flags.c) {
-        console.log('lexicon entries: '+grammar_analyser.count_lexicon_entries())
+        process.stdout.write('Lexicon entries: '+grammar_analyser.count_lexicon_entries()+'\n')
+      }
+      if (cli.flags.d) {
+        process.stdout.write('Duplicate lexicon entries:\n'+grammar_analyser.format(grammar_analyser.find_duplicate())+'\n')
       }
     } else if (cli.input.length==2) {
       var parser = parser_builder(parse(fs.readFileSync(cli.input[0], 'utf8')))
@@ -79,7 +85,8 @@ try {
 
 //*
 } catch(e) {
-  process.stderr.write(chalk.magenta('ERROR:'+e.message+'\n'))
+  if (! ('S' in cli.flags)) process.stderr.write(chalk.magenta('ERROR:'+e.message+'\n'))
+  else process.stderr.write(chalk.magenta(e.stack+'\n'))
   process.exit(1)
 //*/
 } finally {
